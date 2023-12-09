711b317b2ad9fe15b7631eab444051e4d4c5a717
Omar <145347533+OmarAlkhulaif@users.noreply.github.com>
2023-12-10 00:40:14 +0300

    Update User.java

---
+++ src/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private String user_name, password;
+    private int id;
+
+    public User(String user_name, String password, int id){
+        this.user_name = user_name;
+        this.password = password;
+        this.id = id;
+        
+    }
+}
