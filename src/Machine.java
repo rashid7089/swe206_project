30bdfa850b5ec634eee93035caf070d8a2551429
Marwanshtine <eng.mrwanm@gmail.com>
2023-12-11 17:17:26 +0300

    Created the Reservation class
    added the reservation class

--- src/Machine.java
+++ src/Machine.java
@@ -4,11 +4,18 @@ import java.util.ArrayList;
 public class Machine {
     
     private String machineName;
-    private ArrayList<Date> machineSchedule;
+    private ArrayList<Reservation> machineSchedule;
 
-    public Machine(String machineName, ArrayList<Date> machineSchedule){
+    public Machine(String machineName){
         this.machineName = machineName;
-        this.machineSchedule = machineSchedule;
-
+        machineSchedule = new ArrayList<>();
+    }
+    public boolean reserve(Project p, Date s, Date f) {
+        Reservation r = new Reservation(p, s, f);
+        for(Reservation rs: machineSchedule)
+            if(r.conflict(rs))
+                return false;
+        machineSchedule.add(r);
+        return true;
     }
 }
