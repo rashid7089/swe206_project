29754452223ed529870cd87c442b357c04abf05e
rashid7089 <60935992+rashid7089@users.noreply.github.com>
2023-12-11 20:24:58 +0300

    template

---
+++ src/main/java/frontend/Constants.java
@@ -0,0 +1,6 @@
+package frontend;
+
+public abstract class Constants {
+    final static public int laptopWidth = 1366;
+    final static public int laptopHeight = 768;
+}
---
+++ src/main/java/frontend/Template.java
@@ -0,0 +1,43 @@
+package frontend;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Template extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setWidth(Constants.laptopWidth);
+        primaryStage.setHeight(Constants.laptopHeight);
+
+        primaryStage.setTitle("JavaFX Button Template");
+
+        // Create a Button
+        Button myButton = new Button("Click me!");
+
+        // Set an action when the button is clicked
+        myButton.setOnAction(e -> {
+            System.out.println("Button clicked!");
+            // Add your custom action here
+        });
+
+        // Create a layout and add the button to it
+        StackPane root = new StackPane();
+        root.getChildren().add(myButton);
+
+        // Create the scene
+        Scene scene = new Scene(root, 300, 250);
+
+        // Set up the stage
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
